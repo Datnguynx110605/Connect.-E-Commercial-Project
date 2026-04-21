67d6b7d3c50b40c9809eca34e8c99ef1ad113cc9
Nguyen Tien Dat <ntiendat514@gmail.com>
2026-04-21 16:54:57 +0700

    fix UpdateUserPasswordCommand

--- Connect.Application/Features/Users/Commands/UpdateUserPassword/UpdateUserPasswordCommand.cs
+++ Connect.Application/Features/Users/Commands/UpdateUserPassword/UpdateUserPasswordCommand.cs
@@ -7,7 +7,7 @@ namespace Connect.Application.Features.Users.Commands.UpdateUserPassword
 {
     public sealed record UpdateUserPasswordCommand:IRequest<string>
     {
-        public string OldPassword { get; }
+        public string OldPassword { get; init; }
         public string Password { get; init; }
     }
 }
